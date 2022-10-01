5f5811ff3e4b5ef513dc0ae8672c1023ee8eb8b3
Jumpsus <peerapat.rs@gmail.com>
2022-10-01 20:20:03 +0700

    add test case

--- 8_MODULE/ex02/main.cpp
+++ 8_MODULE/ex02/main.cpp
@@ -2,18 +2,32 @@
 
 int main(){
     MutantStack<int> m;
+    MutantStack<int> mcpy(m);
 
     for (int    i = 0; i < 10; i++){
         m.push(i * i);
     }
 
     MutantStack<int>::iterator iter;
+    mcpy = m;
+
+    for (int    i = 10; i < 20; i++){
+        mcpy.push(i * i);
+    }
+
+    std::cout << "m mutant stack member : ";
     iter = m.begin();
     while (iter != m.end()){
         std::cout << (*iter) << " ";
         iter++;
     }
-
     std::cout << std::endl;
 
+    std::cout << "mcpy mutant stack member : ";
+    iter = mcpy.begin();
+    while (iter != mcpy.end()){
+        std::cout << (*iter) << " ";
+        iter++;
+    }
+    std::cout << std::endl;
 }
