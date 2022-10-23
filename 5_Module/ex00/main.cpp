e1902fd782e2d2a960ecba953165e003082f8dde
Jumpsus <peerapat.rs@gmail.com>
2022-10-23 21:58:08 +0700

    add scope

--- 5_Module/ex00/main.cpp
+++ 5_Module/ex00/main.cpp
@@ -1,27 +1,30 @@
 #include "Bureaucrat.hpp"
 
 int main(void){
-    std::cout << "===== First Try (expect exception too high)  =====" << std::endl;
-    try {
-         Bureaucrat b1("bureacrat_1", 100);
-         Bureaucrat b2(b1);
-         b2.Increment(10);
-         std::cout << b2 << std::endl;
-         b2.Increment(1000);
+    {
+        std::cout << "===== First Try (expect exception too high)  =====" << std::endl;
+        try {
+             Bureaucrat b1("bureacrat_1", 100);
+             Bureaucrat b2(b1);
+             b2.Increment(10);
+             std::cout << b2 << std::endl;
+             b2.Increment(1000);
+        }
+        catch (std::exception &exception){
+            std::cout << exception.what() <<std::endl;
+        }
     }
-    catch (std::exception &exception){
-        std::cout << exception.what() <<std::endl;
-    }
-
-    std::cout << std::endl << "===== Second Try (expect exception too low) =====" << std::endl;
-    try {
-         Bureaucrat b1("bureacrat_1", 1);
-         Bureaucrat b2("bureacrat_2", 2);
-         b2 = b1;
-         std::cout << b2 << std::endl;
-         b2.Decrement(1000);
-    }
-    catch (std::exception &exception){
-        std::cout << exception.what() <<std::endl;
+    {
+        std::cout << std::endl << "===== Second Try (expect exception too low) =====" << std::endl;
+        try {
+             Bureaucrat b1("bureacrat_1", 1);
+             Bureaucrat b2("bureacrat_2", 2);
+             b2 = b1;
+             std::cout << b2 << std::endl;
+             b2.Decrement(1000);
+        }
+        catch (std::exception &exception){
+            std::cout << exception.what() <<std::endl;
+        }
     }
 }
